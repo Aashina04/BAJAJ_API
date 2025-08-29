73beb10354b2db4d8c9bd72c48a8296d195356fe
Aashina Saxena <aashinasaxena14@gmail.com>
2025-08-29 11:34:01 +0530

    Updated

--- index.js
+++ index.js
@@ -65,4 +65,4 @@ app.post("/bfhl", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
